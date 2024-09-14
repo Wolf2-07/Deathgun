2f30860b0c7690fd87b20649a11fdaa30990ef6e
Akashi-242 <teamsasaki1@yahoo.com>
2024-09-14 20:02:12 +0100

    Add files via upload
    Signed-off-by: Akashi-242 <teamsasaki1@yahoo.com>

---
+++ config.js
@@ -0,0 +1,84 @@
+//AKASHI
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+
+//=======[dependencies]====================//
+global.SESSION_ID = process.env.SESSION_ID || "";
+global.MONGODB = process.env.MONGODB_URI || "";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.sudo = process.env.SUDO
+  ? process.env.SUDO.replace(/[\s+]/g, "")
+  : "22898555306";
+global.owner = process.env.OWNER_NUMBER
+  ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
+  : "242067274660";
+global.THUMB_IMAGE =
+  process.env.THUMB_IMAGE ||"https://i.imgur.com/JSIcE3I.jpeg"
+  process.env.IMAGE ||
+  "https://i.imgur.com/JSIcE3I.jpeg";
+global.userImages =
+  process.env.USER_IMAGES ||
+  "https://i.imgur.com/JSIcE3I.jpeg,https://i.imgur.com/MTpI6aL.jpeg";
+///===========[global iMPORTS]====================//
+
+module.exports = {
+  menu: process.env.MENU || "",
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "`©𝐷𝑒𝑎𝑡𝒉𝑠𝑔𝑢𝑛`",
+  author: process.env.PACK_AUTHER || "𝐷𝑒𝑎𝑡𝒉𝑠𝑔𝑢𝑛",
+  packname: process.env.PACK_NAME || "𝑺𝒂𝒔𝒂𝒌𝒊 𝑨𝒌𝒂𝒔𝒉𝒊",
+  botname: process.env.BOT_NAME || "𝐷𝑒𝑎𝑡𝒉𝑠𝑔𝑢𝑛",
+  ownername: process.env.OWNER_NAME || "Akashi Sasaki",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "WhatsApp").toUpperCase(),
+};
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "teamsasaki1@gmail.com";
+global.location = "Africa,france";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Douala";
+global.github = process.env.GITHUB || "https://github.com/Deathsgun0001/Deathgun";
+global.gurl = process.env.GURL || "";
+global.website = process.env.GURL || "";
+global.devs = "242067274660";
+global.msg_style = process.env.STYLE || "4";
+global.session_reset = process.env.SS_RESET || "false";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+(global.disablegroup = process.env.DISABLE_GROUPS || "false"),
+  (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
+global.waPresence = process.env.WAPRESENCE || "online";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
+global.read_status_from = process.env.READ_STATUS_FROM || "null";
+global.api_smd = "https://api-smd-1.vercel.app";
+global.scan = "https://sasaki-akashi-session.onrender.com";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
